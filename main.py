0183fad71c0dfe846fae5b9d1b8108193956f30e
kevinkimanzi <kevinkimanzi4@gmail.com>
2024-01-26 19:19:58 +0300

    added my main.py file

---
+++ main.py
@@ -0,0 +1,49 @@
+# variable = a container for a value
+#string
+first_name = "Kevin "
+second_name = "Kimanzi"
+last_name = "Kilonzi"
+full_name = first_name + " "+ second_name +" "+last_name
+
+print("Hello " +full_name)
+
+#integer
+age = 21
+age += 1
+
+print (age)
+
+# print name alongside age
+#can only concatenate str (not "int") to str
+
+print("Your age is" +" " +full_name +" " +str(age))
+
+#FLOAT
+height = 100.5 
+print ("you are of" +" " +str(height) +" " +"of height")
+
+#boolean
+
+human = False
+
+print ("Are you a human:" +" " +str(human))
+
+#Multiple assignment 
+# -allow us to assign multiple variable
+
+name, age, height = "Kevin Kimanzi", 22, 100.5
+
+kevin =karis = leah = 30
+print(kevin)
+print(name)
+
+# STRING METHODS
+
+print(len(name))
+
+#capitalize first letter
+print(name.capitalize())
+
+#uppercase and lowercase
+print(name.upper())
+print(name.lower())
